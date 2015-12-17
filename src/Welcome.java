c938e9500db952dc6d3b8b6af1864a0ff584731d
emielt2 <emielt@gmail.com>
2015-12-17 22:13:07 +0100

    Welcome+secondline

--- src/Welcome.java
+++ src/Welcome.java
@@ -4,5 +4,6 @@
 public class Welcome {
     public static void main(String args[]){
         System.out.println("Welcome to RealProjectOne");
+        System.out.println("Second text line");
     }
 }
